ade908d00e07b433ebc33b4618523e89da4980d8
Joseph Manley <j@cloudsumu.com>
2020-08-06 18:54:58 -0400

    Create get_tasks lambda

---
+++ lambda/get_tasks/lambda_function.py
@@ -0,0 +1,11 @@
+import os, logging, boto3
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
+
+def lambda_handler(event, context):
+    return "OK"
