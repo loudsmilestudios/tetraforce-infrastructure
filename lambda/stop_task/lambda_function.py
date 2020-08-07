beffdcf437834ba4e2f8768ef9d6152fa77e8762
Joseph Manley <j@cloudsumu.com>
2020-08-07 17:01:40 -0400

    Implement stop task lambda

---
+++ lambda/stop_task/lambda_function.py
@@ -0,0 +1,41 @@
+import os, logging, boto3, json
+
+if __name__ != "__main__":
+    from aws_xray_sdk.core import xray_recorder
+    from aws_xray_sdk.core import patch_all
+    patch_all()
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ecs = boto3.client('ecs')
+
+def lambda_handler(event, context):
+    
+    if not 'queryStringParameters' in event or not 'server' in 'queryStringParameters':
+        return build_response("Stop task requires 'server' parameter!", False)
+
+    response = {}
+    
+    response = ecs.stop_task(
+        cluster=os.environ.get("CLUSTER"),
+        task=event['queryStringParameters']['server'],
+        reason="Requested stop from public API endpoint"
+    )
+    
+    if 'tasks' in response and len(response['tasks']) > 0:
+    
+        task = response['tasks'][0]['taskArn']
+        waiter = ecs.get_waiter('tasks_stopped')
+        waiter.wait(tasks=[task], cluster=os.environ.get("CLUSTER"))
+        
+        return build_response("Tasks have successfully stopped!", True)
+    else:
+        logger.error(response)
+        return unknown_error_response()
+
+def unknown_error_response():
+    return build_response("An unknown error occured!", False)
+    
+def build_response(message, success = False):
+    return {"message" : message, "success" : success}
